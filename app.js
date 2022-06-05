b5729dcbb8fadeb5e044687930d1595df17ddb32
Sharthak <sharthakrai@outlook.com>
2022-06-05 21:10:01 +0100

    p

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 
-
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
 
 const query = "London";
 const apiKey = "c190c5b9bac354e1d88c10612a678d9f"
@@ -22,9 +27,22 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
-            res.send("The temperature in London is " + temp + " and the weather is " + weatherDescription);
+            const icon = weatherData.weather[0].icon;
+            const imageURL = `http://openweathermap.org/img/wn/' + icon + '.png'`;
+            res.write("<p>The current temperature in " + query + " is " + temp + " degrees</p>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
+
+      
         });
     });
+
+    console.log(req.body.name);
 });
+
+
+
+
 
 
